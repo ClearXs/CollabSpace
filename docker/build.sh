88cc39ff325f2386ce9c80f8ba2e4e89f0cf6634
j.x <jiangw1027@gmail.com>
2025-03-15 18:41:57 +0800

    feat: Add office-api submodule and simplify build script

--- docker/build.sh
+++ docker/build.sh
@@ -4,17 +4,10 @@ VERSION=$(grep VERSION= .env | cut -d '=' -f2)
 REGISTRY=$(grep REGISTRY= .env | cut -d '=' -f2)
 
 function build() {
-  docker login -u admin -p Harbor12345 192.168.5.201:7080
-
   # web
   docker build --platform linux/arm64 -t ${REGISTRY}/document-web:${VERSION} .
-
-  docker push ${REGISTRY}/document-web:${VERSION}
-
   # service
   docker build --platform linux/x86_64 -t ${REGISTRY}/office-service:${VERSION} .
-
-  docker push ${REGISTRY}/document:${VERSION}
 }
 
 function save() {
